c2a497f16d50f4ca77bfb919d07925caebdbb464
Sara Garrofe <s.garrofe@gmail.com>
2025-10-04 22:09:50 +0200

    fix(navbar): correct links to pages on navbar component

--- portfolio/src/components/Navbar/Navbar.tsx
+++ portfolio/src/components/Navbar/Navbar.tsx
@@ -1,4 +1,5 @@
-import { Navbar as BootstrapNavbar, Nav, Container, Navbar } from 'react-bootstrap';
+import { Nav, Container, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 export default function NavbarComponent() {
     return (
@@ -6,9 +7,9 @@ export default function NavbarComponent() {
             <Navbar data-bs-theme="light" className='mt-5'>
                 <Container>
                     <Nav className="me-auto gap-3">
-                        <Nav.Link href="#home" className='mx-3'>Home</Nav.Link>
-                        <Nav.Link href="#about" className='mx-3'>About me</Nav.Link>
-                        <Nav.Link href="#project" className='mx-3'>Projects</Nav.Link>
+                        <Nav.Link as={Link} to="/home" className='mx-3'>Home</Nav.Link>
+                        <Nav.Link as={Link} to="/about" className='mx-3'>About me</Nav.Link>
+                        <Nav.Link as={Link} to="/projects" className='mx-3'>Projects</Nav.Link>
                     </Nav>
                 </Container>
             </Navbar>
