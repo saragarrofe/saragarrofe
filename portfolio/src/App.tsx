b80f8429c8c471ad964f48011207824ad4b20211
Sara Garrofe <s.garrofe@gmail.com>
2025-10-04 22:06:38 +0200

    feat(routes): add and manage basic routes to pages

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,12 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
+
 import Home from './Pages/Home'
+import About from './Pages/About'
+import Projects from './Pages/Projects'
 
 function App() {
   
 
   return (
     <>
-      <Home />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
+      </Router>
     </>
   )
 }
