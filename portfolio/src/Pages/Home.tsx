25d02c84bd9cc787a98cbb90f9bcfb5779e77754
Sara Garrofe <s.garrofe@gmail.com>
2025-10-04 22:05:57 +0200

    feat(pages): create About, Home and Projects pages

--- portfolio/src/Pages/Home.tsx
+++ portfolio/src/Pages/Home.tsx
@@ -1,11 +1,10 @@
-import { Nav } from "react-bootstrap";
 import NavbarComponent from "../components/Navbar/Navbar";
 
 
 export default function Home() {
     return (
         <>
-            <h1>My Portfolio</h1>
+            <h1>Hello, I'm Sara</h1>
             <NavbarComponent />
         </>
     )
