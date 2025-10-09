74ee1265befadee5a3cb2d8e5b61c3313d6364cb
Sara Garrofe <s.garrofe@gmail.com>
2025-10-09 21:39:45 +0200

    fix(contact): change modal contact to layout component

--- portfolio/src/Pages/Home.tsx
+++ portfolio/src/Pages/Home.tsx
@@ -5,20 +5,9 @@ import Contact from "../components/Contact/Contact";
 
 export default function Home() {
 
-    const [showModal, setShowModal] = useState(false);
-
-    const handleOnContactClick = () => {
-        setShowModal(true);
-    }
-
-    const handleClose = () => {
-        setShowModal(false);
-    }
     return (
         <>
-            <h1>Hello, I'm Sara</h1>
-            <NavbarComponent  onContactClick={handleOnContactClick}/>
-            <Contact show={showModal} handleClose={handleClose} />    
+            <h1>Hello, I'm Sara</h1> 
         </>
     )
 }
--- portfolio/src/components/Layout/Layout.tsx
+++ portfolio/src/components/Layout/Layout.tsx
@@ -1,14 +1,27 @@
+import { useState } from "react";
 import NavbarComponent from "../Navbar/Navbar";
 import { Outlet } from "react-router-dom";
+import Contact from "../Contact/Contact";
 
 export default function Layout() {
+
+  const [modal, setModal] = useState(true);
+
+  const onContactClick = () => {
+    setModal(true);
+  }
+
+  const handleClose = () => {
+    setModal(false);
+  }
+
   return (
     <>
-    {/* Fix el onContactClick */}
-      <NavbarComponent onContactClick={() => { }} />
+      <NavbarComponent onContactClick={onContactClick} />
       <main className="p-4" style={{ marginLeft: 240 }}>
         <Outlet />
       </main>
+      <Contact show={modal} handleClose={handleClose} />
     </>
   );
 }
