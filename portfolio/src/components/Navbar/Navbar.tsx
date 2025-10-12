bf6c96ea8bd3c22fa9194a023a9bb5939007550d
Sara Garrofe <s.garrofe@gmail.com>
2025-10-12 15:06:51 +0200

    feat(navbar): add styles and replace Link with Navlink to use isActive on nav items

--- portfolio/src/components/Navbar/Navbar.tsx
+++ portfolio/src/components/Navbar/Navbar.tsx
@@ -1,5 +1,5 @@
 import { Nav, Navbar } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './Navbar.css'
 
 type NavbarComponentProps = {
@@ -13,10 +13,10 @@ export default function NavbarComponent({ onContactClick }:  NavbarComponentProp
     className="bg-body-tertiary position-fixed top-0 start-0 vh-100 p-3 border-end d-flex flex-column justify-content-center align-items-start sidebar"
     >
         <Nav className="flex-column gap-3 text-start px-3">
-            <Nav.Link as={Link} to="/" className="px-2 py-2 w-100"><i className="bi bi-house me-2"></i>Home</Nav.Link>
-            <Nav.Link as={Link} to="/about" className="px-2 py-2 w-100"><i className="bi bi-person me-2"></i>About me</Nav.Link>
-            <Nav.Link as={Link} to="/projects" className="px-2 py-2 w-100"><i className="bi bi-code me-2"></i>Projects</Nav.Link>
-            <Nav.Link onClick={onContactClick} className="px-2 py-2 w-100"><i className="bi bi-chat-dots me-2"></i>Contact</Nav.Link>
+            <NavLink to="/" end className={({isActive}) => `nav-link d-block px-2 py-2 w-100 ${isActive ? "active" : ""}`}><i className="bi bi-house me-2"></i>Home</NavLink>
+            <NavLink to="/about" className={({isActive}) => `nav-link d-block px-2 py-2 w-100 ${isActive ? "active" : ""}`}><i className="bi bi-person me-2"></i>About me</NavLink>
+            <NavLink to="/projects" className={({isActive}) => `nav-link d-block px-2 py-2 w-100 ${isActive ? "active" : ""}`}><i className="bi bi-code me-2"></i>Projects</NavLink>
+            <button type='button' onClick={onContactClick} className="nav-link d-block text-start px-2 py-2 w-100 btn btn-link"><i className="bi bi-chat-dots me-2"></i>Contact</button>
         </Nav>
     </Navbar>
 
